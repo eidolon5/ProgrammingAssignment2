55d17d4368c1040d2688e2dd0d8e72577fa1e289
eidolon5 <eidolon5.0@gmail.com>
2020-10-16 14:31:09 -0400

    V2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# R Programming Assignment 2
+# Description: 
+# makeCacheMatrix: creates an object that can cache the inverse of a matrix
+# cacheSolve: computes the inverse of the object from makeCacheMatrix. If the 
+# inverse was previously computed, cacheSolve display the message "getting cached
+# data" and will skip the computation. If the inverse was not previously computed,
+# cacheSolve will calculate the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x 
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
+  #m <- NULL
+  #set <- function(y) {
+  #  x <<- y
+  #  m <<- NULL
+  #}
+  #get <- function() x
+  #setmean <- function(mean) m <<- mean
+  #getmean <- function() m
+  #list(set = set, get = get, setmean = setmean, getmean = getmean)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  #print(m)
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
+  
+  #m <- x$getmean()
+  #if(!is.null(m)) {
+  #  message("getting cached data")
+  #  return(m)
+  #}
+  #data <- x$get()
+  #m <- mean(data, ...)
+  #x$setmean(m)
+  #m
 }
